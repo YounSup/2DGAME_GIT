6d80dbf36691e170fc3942338d878fb511ee39c5
YounSup <31726740+YounSup@users.noreply.github.com>
2017-10-06 21:16:47 +0900

    인공지능2

--- 과제/main_state.py
+++ 과제/main_state.py
@@ -14,63 +14,63 @@ class Grass:
   self.image.draw(400, 30)
 
 class Boy:
-image = None
-LEFT_RUN, RIGHT_RUN = 0.1
- def __init__(self):
-  self.x, self.y = random.randint(100, 700), random.randint(100,400)
-  self.frame = random.randint(0,7)
-  slef.dir =1
-  self.state = self.RIGHT_RUN
-  if Boy.image == None :
-      Boy.image = load_image('animation_sheet.png')
-  self.select = False;
-  
- def update(self):
-  if self.state == self.RIGHT_RUN:
-      self.frame = (self.frame + 1) % 8
-      self.x += (self.dir * 5)
-  elif self.state == self.LEFT_RUN:
-      self.frame = (self.frame + 1) % 8
-      self.x += (self.dir * 5)
+    image = None
+    LEFT_RUN, RIGHT_RUN = 0,1
+    def __init__(self):
+      self.x, self.y = random.randint(100, 700), random.randint(100,400)
+      self.frame = random.randint(0,7)
+      slef.dir =1
+      self.state = self.RIGHT_RUN
+      if Boy.image == None :
+          Boy.image = load_image('animation_sheet.png')
+      self.select = False;
       
-  if self.x >800:
-      self.dir= -1
-      self.x = 800
-      self.state = self.LEFT_RUN
-    elif self.x <0:
-        self.dir =1
-        delf.x = 0
-        self.state = self.RIGHT_RUN
+    def update(self):
+      if self.state == self.RIGHT_RUN:
+          self.frame = (self.frame + 1) % 8
+          self.x += (self.dir * 5)
+      elif self.state == self.LEFT_RUN:
+          self.frame = (self.frame + 1) % 8
+          self.x += (self.dir * 5)
+          
+      if self.x >800:
+          self.dir= -1
+          self.x = 800
+          self.state = self.LEFT_RUN
+      elif self.x <0:
+            self.dir =1
+            delf.x = 0
+            self.state = self.RIGHT_RUN
         
- def draw(self):
-  self.image.clip_draw(self.frame*100,self.state*100, 100, 100, self.x, self.y)
+    def draw(self):
+      self.image.clip_draw(self.frame*100,self.state*100, 100, 100, self.x, self.y)
 
-def enter():
-    global boy, grass
-    grass = Grass()
-    boy = Boy()
-
-
-def exit():
-    global boy, grass
-    del(boy)
-    del(grass)
-
-def handle_events():
-    events = get_events()
-    for event in events:
-        if event.type == SDL_QUIT:
-            game_framework.quit()
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-            game_framework.change_state(title_state)
+    def enter():
+        global boy, grass
+        grass = Grass()
+        boy = Boy()
 
 
-def update():
-    boy.update()
-def draw():
-    clear_canvas()
-    grass.draw()
-    boy.draw()
-    update_canvas()
+    def exit():
+        global boy, grass
+        del(boy)
+        del(grass)
+
+    def handle_events():
+        events = get_events()
+        for event in events:
+            if event.type == SDL_QUIT:
+                game_framework.quit()
+            elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+                game_framework.change_state(title_state)
+
+
+    def update():
+        boy.update()
+    def draw():
+        clear_canvas()
+        grass.draw()
+        boy.draw()
+        update_canvas()
 
 
