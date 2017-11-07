33512090fc48aa827b71f30003ba92f6c18166fc
YounSup <31726740+YounSup@users.noreply.github.com>
2017-11-08 00:49:30 +0900

    Rein

--- 텀프로젝트/적test.py
+++ 텀프로젝트/적test.py
@@ -3,32 +3,85 @@ from pico2d import *
 open_canvas(1200,600)
 
 
+LEFT, RIGHT = 0 ,1
 
 class Robot:
     image = [None, None]
     def __init__(c):
         c.x, c.y, c.z = 300,60,0
         c.damage, c.speed = 0,0
-        c.state= 1 #idle
+        c.state= 0
+        c.dir = 1
         c.frame =0
+        c.count =0
         if Robot.image[0] == None or Robot.image[1] == None :
           Robot.image[0] = load_image('쫄따구.png')
           Robot.image[1] = load_image('쫄따구오른쪽.png')
-
+          
+    def idle(c):
+        if c.dir == RIGHT :# 오른쪽
+            c.x += 5
+            c.count +=1
+            if c.count == 10:
+                c.count =0
+                c.dir =LEFT
+        elif c.dir == LEFT:
+            c.x -= 5
+            c.count +=1
+            if c.count == 10:
+                c.count =0
+                c.dir =RIGHT
+                
     def update(c):
         c.frame = (c.frame+1)%4
-        c.x +=2
-        if c.x >= 500:
-            c.x = 0
-            c.state = 0
+        c.idle()
+        
     def draw(c):
-            c.image[c.state].clip_draw(c.frame * 90,0,90,86,c.x,c.y)
+            c.image[c.dir].clip_draw(c.frame * 90,0,90,86,c.x,c.y)
+
+class Reinhard:
+    image = None
+    def __init__(c):
+        c.x,c.y,c.z = 500, 250, 0
+        c.damage, c.speed = 0,0
+        c.state= 0
+        c.dir = 1
+        c.frame =0
+        c.count =0
+        if Reinhard.image == None:
+            Reinhard.image = load_image('라인하르트.png')
+    def idle(c):
+        if c.dir == RIGHT :
+            c.x += 5
+            c.count +=1
+            if c.count == 10:
+                c.count =0
+                c.dir =LEFT
+        elif c.dir == LEFT:
+            c.x -= 5
+            c.count +=1
+            if c.count == 10:
+                c.count =0
+                c.dir =RIGHT
+    def update(c):
+        c.frame = (c.frame+1)%2
+        c.idle()
+        
+    def draw(c):
+        c.image.clip_draw(c.frame * 199, c.dir * 177 ,199,177,c.x,c.y)
+            
+    
 
 e= Robot()
+f= Reinhard()
+
 a = True;
+
 while a:
     clear_canvas()
     e.update()
+    f.update()
     e.draw()
+    f.draw()
     update_canvas()
-    delay(0.01)
+    delay(0.05)
